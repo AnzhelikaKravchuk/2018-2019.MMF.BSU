8e13263ce9ae4532deec13df4cf5218534133832
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2018-10-24 20:45:19 +0300

    Add maze task

---
+++ 3 course/24.10.2018/MazeTask/MazeLibrary/MazeSolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MazeLibrary
+{
+    public class MazeSolver
+    {
+        public MazeSolver(int[,] mazeModel, int startX, int startY) => throw new NotImplementedException();
+
+        public int[,] MazeWithPass() => throw new NotImplementedException();
+
+        public void PassMaze() => throw new NotImplementedException();
+    }
+}
